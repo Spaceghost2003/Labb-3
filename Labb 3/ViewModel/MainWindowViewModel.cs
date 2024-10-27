91a07893776fb5a9cc5e576b20ffbd386665df74
Jonathan Olaussen Alvin <jonathanoalvin@gmail.com>
2024-10-27 21:42:17 +0100

    Add project files.

---
+++ Labb 3/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_3.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<QuestionPackViewModel> Packs { get; set; }
+
+		public ConfigurationViewModel ConfigurationViewModel { get; }
+        public PlayerViewModel PlayerViewModel { get;}
+
+        private QuestionPackViewModel? _activePack;
+
+		public QuestionPackViewModel? ActivePack
+		{
+			get => _activePack;
+			set 
+			{
+				_activePack = value;
+				RaisePropertyChanged();
+				ConfigurationViewModel.RaisePropertyChanged("ActivePack");
+			}
+		}
+
+		public MainWindowViewModel()
+		{
+			ConfigurationViewModel = new ConfigurationViewModel(this);
+			PlayerViewModel = new PlayerViewModel(this);
+
+			ActivePack = new QuestionPackViewModel(new Model.QuestionPack("My Question Pack"));
+        }
+
+	}
+}
---
+++ Labb 3/ViewModel/PlayerViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Labb_3.Command;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Labb_3.ViewModel
+{
+    internal class PlayerViewModel : ViewModelBase
+    {
+        public string TestData {
+            get => _testData;
+            private set 
+            {
+                _testData = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DelegateCommand UpdateButtonCommand { get; }
+
+        private readonly MainWindowViewModel? mainWindowViewModel;
+
+        private DispatcherTimer timer;
+        private string _testData;
+
+        public PlayerViewModel(MainWindowViewModel? mainWindowViewModel)
+        {
+            this.mainWindowViewModel = mainWindowViewModel;
+
+            TestData = "Start value";
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            //timer.Start();
+            UpdateButtonCommand = new DelegateCommand(UpdateButton);
+
+        }
+
+
+
+        private void UpdateButton(object obj)
+        {
+            TestData += "x";
+            UpdateButtonCommand.RaiseCanExecuteChanged();
+        }
+
+        private void Timer_Tick(Object? sender, EventArgs e)
+        {
+            TestData += "x";
+        }
+    }
+}
